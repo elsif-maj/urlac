012ed4ed7d0fd050e01e7e05738e20caf37850a4
Michael Johnson <elsif.maj@gmail.com>
2024-02-03 13:30:05 -0800

    getting close to working on an Umbra branch instead of here

--- react/src/components/AudioChatButton.jsx
+++ react/src/components/AudioChatButton.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react"
 import audioChatConnect from "../services/audioChat"
+import {
+  Tooltip,
+  IconButton,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { PhoneIcon } from "@chakra-ui/icons";
 
 const AudioChatButton = () => {
   const [urlacIsOn, setUrlacIsOn] = useState(false)
@@ -20,9 +26,30 @@ const AudioChatButton = () => {
   }
 
   return (
-    <button onClick={handleAudioChatToggleButton}>
-      Audio Chat
-    </button>
+    // O.g.:
+    // <button onClick={handleAudioChatToggleButton}>
+    //   Audio Chat
+    // </button>
+
+    <Tooltip
+      label='Click to copy room URL'
+      fontSize='md'
+      bg={useColorModeValue("orange.200", "orange.900")}
+      color={useColorModeValue("gray.600", "white")}
+    >
+    <IconButton
+        icon={<PhoneIcon color='white' />}
+        ml={6}
+        onClick={handleAudioChatToggleButton}
+        variant='solid'
+        bg='linear-gradient(225deg, hsla(184, 100%, 54%, 1) 0%, hsla(210, 100%, 50%, 1) 100%)'
+        aria-label='Copy room URL'
+        _hover={{
+          bg: "linear-gradient(225deg, hsla(210, 100%, 50%, 1) 0%, hsla(184, 100%, 54%, 1) 100%)",
+        }}
+        textAlign='end'
+      />
+    </Tooltip>
   )
 }
 
