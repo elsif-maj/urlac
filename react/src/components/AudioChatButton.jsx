643d59df71acc1d7713da83ac68e3dbc245b485c
Michael Johnson <elsif.maj@gmail.com>
2024-02-03 17:57:48 -0800

    updates

--- react/src/components/AudioChatButton.jsx
+++ react/src/components/AudioChatButton.jsx
@@ -1,3 +1,4 @@
+// import React from 'react' // this was necessary when adding to Umbra
 import { useState } from "react"
 import audioChatConnect from "../services/audioChat"
 import {
@@ -15,41 +16,62 @@ const AudioChatButton = () => {
   const handleAudioChatToggleButton = (e) => {
     e.preventDefault()
 
+    const url = new URL(window.location.href);
+    let roomName = url.searchParams.get("room");
+
     audioChatConnect(
       urlacIsOn,
       setUrlacIsOn,
       pc, 
       setPc,
       ws,
-      setWs
+      setWs,
+      roomName
     )
   }
 
   return (
-    // O.g.:
-    // <button onClick={handleAudioChatToggleButton}>
-    //   Audio Chat
-    // </button>
-
-    <Tooltip
-      label='Click to copy room URL'
-      fontSize='md'
-      bg={useColorModeValue("orange.200", "orange.900")}
-      color={useColorModeValue("gray.600", "white")}
-    >
-    <IconButton
-        icon={<PhoneIcon color='white' />}
-        ml={6}
-        onClick={handleAudioChatToggleButton}
-        variant='solid'
-        bg='linear-gradient(225deg, hsla(184, 100%, 54%, 1) 0%, hsla(210, 100%, 50%, 1) 100%)'
-        aria-label='Copy room URL'
-        _hover={{
-          bg: "linear-gradient(225deg, hsla(210, 100%, 50%, 1) 0%, hsla(184, 100%, 54%, 1) 100%)",
-        }}
-        textAlign='end'
-      />
-    </Tooltip>
+    urlacIsOn ? (
+      <Tooltip
+        label='Click to leave voice chat'
+        fontSize='md'
+        bg={useColorModeValue("orange.200", "orange.900")}
+        color={useColorModeValue("gray.600", "white")}
+      >
+        <IconButton
+          icon={<PhoneIcon color='white' />}
+          ml={6}
+          onClick={handleAudioChatToggleButton}
+          variant='solid'
+          bg='linear-gradient(225deg, hsla(184, 100%, 54%, 1) 0%, hsla(210, 100%, 50%, 1) 100%)'
+          aria-label='Leave Voice Chat'
+          _hover={{
+            bg: "linear-gradient(225deg, hsla(210, 100%, 50%, 1) 0%, hsla(184, 100%, 54%, 1) 100%)",
+          }}
+          textAlign='end'
+        />
+      </Tooltip>
+    ) : (
+      <Tooltip
+        label='Click to join voice chat'
+        fontSize='md'
+        bg={useColorModeValue("orange.200", "orange.900")}
+        color={useColorModeValue("gray.600", "white")}
+      >
+        <IconButton
+          icon={<PhoneIcon color='white' />}
+          ml={6}
+          onClick={handleAudioChatToggleButton}
+          variant='solid'
+          bg='linear-gradient(225deg, hsla(184, 100%, 54%, 1) 0%, hsla(210, 100%, 50%, 1) 100%)'
+          aria-label='Join Voice Chat'
+          _hover={{
+            bg: "linear-gradient(225deg, hsla(210, 100%, 50%, 1) 0%, hsla(184, 100%, 54%, 1) 100%)",
+          }}
+          textAlign='end'
+        />
+      </Tooltip>
+    )
   )
 }
 
